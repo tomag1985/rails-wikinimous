5d6a1895785baa4a302835ec6b5ae738463a3036
tomas agnese <tagnese1985@gmail.com>
2021-01-12 15:25:00 -0300

    wikinimous functioning with no style

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,12 @@
 class ArticlesController < ApplicationController
-	
+
+	before_action :find_article, only: [:show, :edit, :update, :destroy]
+
 	def index
 		@articles = Article.all
 	end
+
+	def show; end
 
 	def new
 		@article = Article.new
@@ -13,19 +17,23 @@ class ArticlesController < ApplicationController
 		redirect_to articles_path
 	end
 
-	def edit
-	end
-
-	def show
-	end
+	def edit; end
 
 	def update
+		@article.update(article_params)
+		redirect_to article_path(@article)
 	end
 
 	def destroy
+		@article.destroy
+		redirect_to articles_path
 	end
 
 	private
+
+	def find_article
+		@article = Article.find(params[:id])
+	end
 
 	def article_params
 		params.require(:article).permit(:title, :content)
