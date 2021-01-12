a5bb73cb0cbeb894026d8a5a1e6313056496afa3
tomas agnese <tagnese1985@gmail.com>
2021-01-12 12:19:23 -0300

    controllers, models and views

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,33 @@
 class ArticlesController < ApplicationController
+	
+	def index
+		@articles = Article.all
+	end
+
+	def new
+		@article = Article.new
+	end
+
+	def create
+		@article = Article.new(article_params).save
+		redirect_to articles_path
+	end
+
+	def edit
+	end
+
+	def show
+	end
+
+	def update
+	end
+
+	def destroy
+	end
+
+	private
+
+	def article_params
+		params.require(:article).permit(:title, :content)
+	end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,24 +1,2 @@
 class Article < ApplicationRecord
-
-	def index
-	end
-
-	def create
-	end
-
-	def new
-	end
-
-	def edit
-	end
-
-	def show
-	end
-
-	def update
-	end
-
-	def destroy
-	end
-
 end
